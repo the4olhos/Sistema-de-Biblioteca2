ec07a971c8747037361212d1abbdc4bb092260fd
Fussuma Murilo <168040802+Fussuma-Murilo@users.noreply.github.com>
2024-10-24 21:38:06 -0300

    Update Livro.java

--- src/Livro.java
+++ src/Livro.java
@@ -1,16 +1,21 @@
 public class Livro {
+
+    //Declaração de variáveis
     private String titulo;
     private Autor autor;
-
+    
+    //Método construtor da classe, utilizando um objeto Autor e uma string
     public Livro(String titulo, Autor autor) {
         this.titulo = titulo;
         this.autor = autor;
     }
-
-    public void alterarAltor(Autor autor){
+    
+    //Função para realizar alteração do autor
+    public void alterarAutor(Autor autor){
         this.autor = autor;
     }
 
+    //Método toString
     @Override
     public String toString() {
         return "Livro: " + titulo + autor;
