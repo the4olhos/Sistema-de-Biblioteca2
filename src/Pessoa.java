c4d8bb7e681ece1f9625c669f1da6e59d29a914e
Fussuma Murilo <168040802+Fussuma-Murilo@users.noreply.github.com>
2024-10-24 21:42:12 -0300

    Update Pessoa.java

--- src/Pessoa.java
+++ src/Pessoa.java
@@ -1,10 +1,15 @@
 abstract class Pessoa {
+
+    //Declaração das variáveis
     protected  String nome;
     protected  String email;
 
+    //Método construtor da classe
     public Pessoa(String nome, String email) {
         this.nome = nome;
         this.email = email;
     }
+
+    //Método abstrato para exibição de dados
     public abstract  void exibirDetalhes();
 }
