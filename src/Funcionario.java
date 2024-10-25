5a38995c42009bb1d578b29046bacbe64c21f730
Fussuma Murilo <168040802+Fussuma-Murilo@users.noreply.github.com>
2024-10-24 21:39:01 -0300

    Update Funcionario.java

--- src/Funcionario.java
+++ src/Funcionario.java
@@ -1,13 +1,17 @@
 public class Funcionario extends Pessoa{
 
+    //Declaração das variáveis
     private String matricula;
     private String cargo;
 
+    //Método construtor da classe, utiliza Strings como parâmetros
     public Funcionario(String nome, String email, String matricula, String cargo) {
         super(nome, email);
         this.matricula = matricula;
         this.cargo = cargo;
     }
+
+    //Método toString
     @Override
     public void exibirDetalhes(){
 
