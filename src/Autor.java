ad8ad8050b7289b00f2ca8ac5d58782cdde5c968
Alexksandro Fq <168040540+Alexksandrofq@users.noreply.github.com>
2024-10-24 21:47:02 -0300

    Update Autor.java

--- src/Autor.java
+++ src/Autor.java
@@ -1,19 +1,20 @@
-public class Autor extends Pessoa {
+public class Autor extends Pessoa {//criação da classe Autor que está herdando da classe Pessoas
+    //variável biografia do tipo String que está privada
     private String biografia;
 
-    public Autor(String nome, String email, String biografia) {
+    public Autor(String nome, String email, String biografia) {//construtor
         super(nome, email);
         this.biografia = biografia;
     }
 
     @Override
-    public void exibirDetalhes() {
+    public void exibirDetalhes() {//método
         System.out.println("Autor: " + nome + " - Email: " + email);
         System.out.println("Biografia: " + biografia);
     }
 
     @Override
-    public String toString() {
+    public String toString() {//método
         return "\nAutor: " + nome + "- E-mail: " + email +
                 "\nBiográfia: " + biografia;
     }
