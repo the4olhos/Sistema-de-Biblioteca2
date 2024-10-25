31982b6b752dbeebe43f59ba187514932fe0ee33
Fussuma Murilo <168040802+Fussuma-Murilo@users.noreply.github.com>
2024-10-24 21:36:24 -0300

    Update Reserva.java

--- src/Reserva.java
+++ src/Reserva.java
@@ -1,18 +1,19 @@
 public class Reserva {
 
+    //Declaração de variáveis
     private Livro livro;
     private Leitor leitor;
     private String dataReserva;
 
-
+    //Método construtor da classe, utiliza um livro e um leitor como parâmetro
     public Reserva(Livro livro, Leitor leitor){
 
         this.livro = livro;
         this.leitor = leitor;
         dataReserva = "*Dia de hoje*";
-
     }
 
+    //Método toString da classe
     @Override
     public String toString() {
         return "Data da reserva: " + dataReserva +
