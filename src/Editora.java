1fba0b11ae55a56b76e6d13fff4ecbd14ad0a49f
Fussuma Murilo <168040802+Fussuma-Murilo@users.noreply.github.com>
2024-10-24 21:39:57 -0300

    Update Editora.java

--- src/Editora.java
+++ src/Editora.java
@@ -3,14 +3,17 @@ import java.util.List;
 
 public class Editora {
 
+    //Declaração de variáveis
     private List<Livro> livros;
     private String editora;
 
+    //Método construtor da classe
     public Editora(String editora){
         this.editora = editora;
         this.livros = new ArrayList<>();
     }
 
+    //Método para adicionar livros, demonstrando a associação de objetos
     public void adicionarLivro(Livro livro){
         livros.add(livro);
     }
