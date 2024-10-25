5e0b2c6be3782e67ac30d86348b017f7900b212e
Samuca Sozua <158528391+devsamuca@users.noreply.github.com>
2024-10-24 21:34:42 -0300

    Update Emprestimo.java

--- src/Emprestimo.java
+++ src/Emprestimo.java
@@ -1,11 +1,14 @@
 public class Emprestimo {
+    //Atributos da classe emprestimo
     private Leitor leitor;
     private Livro livro;
 
+    //Construtor da clsse exige um leitor e um livro como parametro
     public Emprestimo(Leitor leitor, Livro livro) {
         this.leitor = leitor;
         this.livro = livro;
     }
+    //Exibe o leitor e o livro do emprestimo
     public void exibirDetalhes() {
         System.out.println("Empréstimo realizado:");
         leitor.exibirDetalhes();
